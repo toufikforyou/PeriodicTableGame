06931408a271f56ce43d03fda82e2f20114ac958
MH TOUFIK <toufik@sopnolikhi.com>
2025-03-23 00:57:50 +0600

    refactor ViewModelModule to use updated Koin DSL for GameViewModel

--- app/src/main/java/dev/toufikforyou/periodictable/di/ViewModelModule.kt
+++ app/src/main/java/dev/toufikforyou/periodictable/di/ViewModelModule.kt
@@ -1,9 +1,11 @@
 package dev.toufikforyou.periodictable.di
 
 import dev.toufikforyou.periodictable.main.presentation.viewmodel.GameViewModel
-import org.koin.androidx.viewmodel.dsl.viewModelOf
+import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
-    viewModelOf(::GameViewModel)
+    viewModel {
+        GameViewModel(get())
+    }
 }
